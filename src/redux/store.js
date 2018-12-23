56f5d227466ce0b9f319953411f74866525caec8
vilisimo <vilisimo@gmail.com>
2018-12-23 13:12:04 +0200

    Enable redux tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default createStore(
   rootReducer,
-  applyMiddleware(thunk),
+  composeEnhancers(applyMiddleware(thunk)),
 );
