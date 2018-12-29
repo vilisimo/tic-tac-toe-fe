e439e7dc72d42cd34ef5a151e4b83bf1107dd515
vilisimo <vilisimo@gmail.com>
2018-12-29 19:37:24 +0200

    Add board component tests

---
+++ src/components/__test__/board.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Board from '../board';
+import Row from '../row';
+
+Enzyme.configure({ adapter: new Adapter() })
+
+const setup = () => ({
+  wrapper: shallow(<Board />),
+});
+
+describe('components', () => {
+  describe('board', () => {
+    it('should render the board', () => {
+      const { wrapper } = setup();
+
+      expect(wrapper.find('.board').hasClass('board')).toBe(true);
+    });
+
+    it('should render rows', () => {
+      const { wrapper } = setup();
+
+      expect(wrapper.find(Row).length).toEqual(3);
+    });
+  });
+});
